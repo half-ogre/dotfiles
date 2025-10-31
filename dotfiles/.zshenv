b258ebd618bcf135af4c13ae18ae4ed1b55cfdf9
half-ogre <half-ogre@users.noreply.github.com>
2025-10-30 22:43:16 -0700

    make utils available elsewhere

--- dotfiles/.zshenv
+++ dotfiles/.zshenv
@@ -1,11 +1,10 @@
 export PATH_ZSHEVN=${PATH}
 
+export DOTFILE_DIR="${${(%):-%x}:A:h:h}"
+
 export XDG_CONFIG_HOME="${HOME}/.config"
 export ZDOTDIR="${XDG_CONFIG_HOME}/zsh"
 
 [ -f "/opt/homebrew/bin/brew" ] && eval "$(/opt/homebrew/bin/brew shellenv)"
 
 export PATH=$PATH:$(go env GOPATH)/bin
-
-
-export PATH=$PATH:$HOME/.dotnet/tools
--- install.sh
+++ install.sh
@@ -1,28 +1,6 @@
 #!/bin/zsh
 
-function install_homebrew_cask {
-  echo ""
-  echo "Checking installed Homebrew casks for $1 ($2) ..."
-  if [ $(brew info --cask $2 | grep -c "Not installed") -eq 1 ]; then
-    echo "Installing $1 ..."
-    brew install --cask $2
-    echo "Homebrew cask $2 is now installed"
-  else
-    echo "Homebrew cask $2 is already installed"
-  fi
-}
-
-function install_homebrew_formulae {
-  echo ""
-  echo "Checking installed Homebrew formulae for $1 ($2) ..."
-  if [ $(brew info $2 | grep -c "Not installed") -eq 1 ]; then
-    echo "Installing Homebrew formulae $1 ..."
-    brew install $2
-    echo "Homebrew formulae $2 is now installed"
-  else
-    echo "Homebrew formulae $2 is already installed"
-  fi
-}
+source "${0:a:h}/scripts/dotfiles-utils.sh"
 
 echo "Install started"
 echo ""
