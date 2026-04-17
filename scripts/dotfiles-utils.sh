d9f35d7325eb5115a11259bc806352ba7e6d0271
half-ogre <half-ogre@users.noreply.github.com>
2026-04-16 20:52:55 -0700

    use brew list for install check; quote package name

--- scripts/dotfiles-utils.sh
+++ scripts/dotfiles-utils.sh
@@ -3,9 +3,9 @@
 function install_homebrew_cask {
   echo ""
   echo "Checking installed Homebrew casks for $1 ($2) ..."
-  if [ $(brew info --cask $2 | grep -c "Not installed") -eq 1 ]; then
+  if ! brew list --cask "$2" &> /dev/null; then
     echo "Installing $1 ..."
-    brew install --cask $2
+    brew install --cask "$2"
     echo "Homebrew cask $2 is now installed"
   else
     echo "Homebrew cask $2 is already installed"
@@ -15,9 +15,9 @@ function install_homebrew_cask {
 function install_homebrew_formulae {
   echo ""
   echo "Checking installed Homebrew formulae for $1 ($2) ..."
-  if [ $(brew info $2 | grep -c "Not installed") -eq 1 ]; then
+  if ! brew list --formula "$2" &> /dev/null; then
     echo "Installing Homebrew formulae $1 ..."
-    brew install $2
+    brew install "$2"
     echo "Homebrew formulae $2 is now installed"
   else
     echo "Homebrew formulae $2 is already installed"
